764126e2ce175648f2d80beea613096f44556572
apu242007 <jorge_e_castro@hotmail.com>
2026-02-18 07:51:57 -0300

    fix: subir ATSForm como ATSForm2

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ import { EmergencyDrillForm } from './components/EmergencyDrillForm';
 import { DailyInspectionCatIForm } from './components/DailyInspectionCatIForm';
 import { DroppedObjectsForm } from './components/DroppedObjectsForm';
 import { TubingMeasurementForm } from './components/TubingMeasurementForm';
-import { ATSForm } from './components/ATSForm';
+import { ATSForm2 as ATSForm } from './components/ATSForm2';
 import { LocationHandoverForm } from './components/LocationHandoverForm';
 
 import { 
